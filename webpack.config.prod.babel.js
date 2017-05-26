1abf798db45365b1556ce43bba2f07710812c20d
Peter Adams <pyssion5@gmail.com>
2017-05-26 08:08:33 +0800

    Add webpack-html plugin. Add basic prod config. Adjust App style.

--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -1,22 +1,14 @@
 const { resolve } = require('path')
 const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const rootDir = __dirname
 const srcDir = resolve(rootDir, 'src')
 const distDir = resolve(rootDir, 'dist')
-// const nodeModulesDir = resolve(rootDir, 'node_modules')
 const appName = 'index.js'
-const port = 2017
 
 const basicConfig = {
   // webpack创建依赖图的入口
-  entry: [
-    'react-hot-loader/patch',
-    `webpack-dev-server/client?http://localhost:${port}`,
-    'webpack/hot/only-dev-server',
-    resolve(srcDir, appName),
-  ],
+  entry: resolve(srcDir, appName),
   // webpack生成打包后文件的处理配置
   output: {
     path: distDir,
@@ -30,22 +22,18 @@ const basicConfig = {
     ],
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin(),
-    new HtmlWebpackPlugin({ template: resolve(srcDir, 'assets/index.tpl') }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+    }),
   ],
   resolve: {
     extensions: ['.js', '.jsx'],
     modules: [srcDir, 'node_modules'],
   },
-  devtool: 'cheap-module-eval-source-map',
-  // will be picked up by webpack-dev-server
-  devServer: {
-    hot: true,
-    contentBase: distDir,
-    historyApiFallback: true,
-    port,
-  },
+  devtool: 'cheap-source-map',
 }
 
 module.exports = basicConfig
