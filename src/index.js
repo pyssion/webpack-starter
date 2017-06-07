aaa312f50b833118e9c7f2d51987474abcbabfc0
Peter Adams <pyssion5@gmail.com>
2017-06-08 00:45:44 +0800

    Feature/basic hmr (#5)
    * Update webpack, webpack-dev-server, and react-hot-loader.
    
    * Add hmr setup for react and redux.
    
    * Update react/jsx-boolean-value rule

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+import React from 'react'
 import { render } from 'react-dom'
-import appRoutes from 'route'
+import { AppContainer } from 'react-hot-loader'
+import appRoutes from './route'
 
-render(
-  appRoutes,
-  document.getElementById('app'),
-)
+const doRender = app => {
+  render(
+    <AppContainer>
+      {app}
+    </AppContainer>,
+    document.getElementById('app'),
+  )
+}
+
+doRender(appRoutes)
+
+if (module.hot) { // eslint-disable-line no-undef
+  module.hot.accept('./route', () => { // eslint-disable-line no-undef
+    doRender(appRoutes)
+  })
+}
